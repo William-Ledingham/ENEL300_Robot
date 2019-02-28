b7652eab9e3e1df4e98e663e516ee15790ad44ed
William Ledingham <williamledingham1@gmail.com>
2019-02-27 22:46:22 -0700

    Basic Setup

--- Robot_Main.c
+++ Robot_Main.c
@@ -1,40 +1,59 @@
 #include "simpletools.h"
 #include "servo.h"
 #include "stdio.h"
+#include "sirc.h"
 
 #include <Robot_Main.h>
 
+/* -- Global Variables -- */
+
+
+
 int main()
 {
-  // Add startup code here.
-
-  fun(); 
+  int state = 0;
+  int remoteCode;
+  sirc_setTimeout(1000); // -1 if no remote code in 1s
+ 
   while(1)
   {
-    /*
-    switch()
+    
+    remoteCode = sirc_button(7);
+    
+    if(remoteCode != -1)
     {
-     case '': //Default Waiting State
+      state = remoteCode;
+    }
+    
+    /*
+    switch(state)
+    {    
+     case : //Emotion 1
      
      break;
      
-     case '': //Emotion 1
+     case ; //Emotion 2
      
      break;
      
-     case ''; //Emotion 2
+     case : //Emotion 3
      
      break;
      
-     case '': //Emotion 3
+     case : //Emotion 4
      
      break;
      
-     case '': //Emotion 4
-     
-     break;
+     default:
      
     }     */ 
     
   }  
+}
+
+
+void setServo(int leftSpeed, int rightSpeed) 
+{
+  servo_speed(12, -leftSpeed);
+  servo_speed(14, rightSpeed);
 }
--- Robot_Main.h
+++ Robot_Main.h
@@ -1,6 +1,10 @@
 #ifndef Robot_Main_h
 #define Robot_Main_h
 
+/* Robot_Main.c ------------------------------------*/
+void setServo(int leftSpeed, int rightSpeed);
+
+/* Emotion_Anger.c ---------------------------------*/
 void fun();
 
 
