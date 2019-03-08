88044d3f83a7ebf86c207ab72ea52674a127695f
ThePiGuy <ThePiGuy@users.noreply.github.com>
2019-03-07 22:42:31 -0700

    Added RGB Led Support
    - Add RGB Led support for all emotions
    - Added macro definitions for all pins
    - Changed printf() to print() for better Serial debugging support

--- Robot_Main.c
+++ Robot_Main.c
@@ -8,6 +8,8 @@
 /* -- Global Variables -- */
  int lastIRCode = 0;
  int emotionalState = 0;
+ 
+ int eyeR, eyeG, eyeB;
 
 
 
@@ -16,7 +18,10 @@ int main()
   
   //int state = 0;
   //int remoteCode;
-  int* coginfo = cog_run(&IRSensorCog, 2000);
+  int* IRCogInfo = cog_run(&IRSensorCog, 128);
+  int* EyeCogInfo = cog_run(&pwmEyeCog, 128);
+  
+  print("Main Started.\n");
  
   while(1)
   {
@@ -24,20 +29,18 @@ int main()
     {    
      case ANGER: //Emotion 1
       AngerFSM();
-
      break;
      
      case FEAR: //Emotion 2
-     
+      FearFSM();
      break;
      
      case SADNESS: //Emotion 3
       SadnessFSM();
-     
      break;
      
      case LOVE: //Emotion 4
-     
+      LoveFSM();
      break;
      
      default:
@@ -52,19 +55,38 @@ int main()
 
 void AngerFSM()
 {
-      printf("Anger Emotion Started.\n");
+      print("Anger Emotion Started.\n");
 
-      freqout(14, 3000, 500);
+      freqout(14, 500, 800);
+      setEyeColors(230, 126, 34);
       
       if (emotionalState != ANGER) return;
       
-      freqout(14, 20000, 500);
+      freqout(14, 500, 500);
+      setEyeColors(128, 128, 0);
 
 
 }  
 
+void FearFSM() {
+  print("Fear Emotion Started.\n");
+  setEyeColors(0, 0, 100);
+}  
+
+
 void SadnessFSM() {
-  printf("Sadness Emotion Started.\n");
+  print("Sadness Emotion Started.\n");
+  setEyeColors(0, 0, 100);
+}  
+
+
+void LoveFSM() {
+  print("Love/Affection Emotion Started.\n");
+  
+  setEyeColors(190, 51, 255);
+  pause(1000);
+  setEyeColors(255, 51, 212);
+  pause(1000);
 }  
 
 void IRSensorCog()
@@ -86,7 +108,7 @@ void IRSensorCog()
     if(remoteCode != -1)
     {
       lastIRCode = remoteCode;
-      printf("New IR Code sensed: %d.\n", lastIRCode);
+      print("New IR Code sensed: %d.\n", lastIRCode);
       
       switch(lastIRCode)
       {    
@@ -122,18 +144,47 @@ void IRSensorCog()
 
 void setServo(int leftSpeed, int rightSpeed) 
 {
-  servo_speed(12, -leftSpeed);
-  servo_speed(14, rightSpeed);
+  servo_speed(SERVO_DRIVE_L, -leftSpeed);
+  servo_speed(SERVO_DRIVE_R, rightSpeed);
 }
 
 /* Arguments take angle from -900 to 900 */
 void setEyebrowAngle(int leftEye, int rightEye)
 { 
-  servo_angle(17,  leftEye + 900);
-  servo_angle(16, 900 - rightEye);
+  servo_angle(SERVO_EYEBROW_L,  leftEye + 900);
+  servo_angle(SERVO_EYEBROW_R, 900 - rightEye);
 }
 
-void setLEDColors(int r, int g, int b)
+void setEyeColors(int r, int g, int b)
 {
-  
+  eyeR = r;
+  eyeG = g;
+  eyeB = b;
 }  
+
+void pwmEyeCog() {
+  int dutyResolutionTime = 1; // in ms
+  
+  while (1) {
+    print("%d %d %d\n", eyeR, eyeG, eyeB);
+    for (int i = 0; i < 255; i++) {
+      if (i >= eyeR) 
+        high(PIN_EYE_R);
+      else 
+        low(PIN_EYE_R);
+        
+      if (i >= eyeG) 
+        high(PIN_EYE_G);
+      else 
+        low(PIN_EYE_G);
+        
+      if (i >= eyeB) 
+        high(PIN_EYE_B);
+      else 
+        low(PIN_EYE_B);
+        
+      //pause(0);
+      
+    } // end for
+  } // end while
+}            
--- Robot_Main.h
+++ Robot_Main.h
@@ -1,22 +1,37 @@
 #ifndef Robot_Main_h
 #define Robot_Main_h
 
+/* Emotional States --------------------------------*/
 #define ANGER 1
 #define SADNESS 2
 #define FEAR 3
 #define LOVE 4
 
+/* Pin Definitions ---------------------------------*/
+#define PIN_EYE_R 2
+#define PIN_EYE_G 1
+#define PIN_EYE_B 0
+
+#define SERVO_DRIVE_L 12
+#define SERVO_DRIVE_R 14
+#define SERVO_EYEBROW_L 16
+#define SERVO_EYEBROW_R 17
+
+
 /* Robot_Main.c ------------------------------------*/
-void enableIRSensorCog();
+void IRSensorCog();
+void pwmEyeCog();
 
 // Sensor and Control Functions
 void setServo(int leftSpeed, int rightSpeed);
 void setEyebrowAngle(int leftEye, int rightEye);
-void setLEDColors(int r, int g, int b);
+void setEyeColors(int r, int g, int b);
 
 
 /* Emotion_Anger.c ---------------------------------*/
 void AngerFSM();
+void FearFSM();
 void SadnessFSM();
+void LoveFSM();
 
 #endif
